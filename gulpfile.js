4831df4df28974ef16c4a6b30ca63a9999179634
Serhii Yakymenko <m.serhiiyakymenko@gmail.com>
2020-05-05 18:58:02 -0400

    New file style.min.css to minimize style.css

--- gulpfile.js
+++ gulpfile.js
@@ -15,3 +15,25 @@ gulp.task('browser-sync', function() {
   });
   gulp.watch("./*.html").on('change', browserSync.reload);
 });
+
+
+let gulp = require('gulp');
+let cleanCSS = require('gulp-clean-css');
+// Task to minify css using package cleanCSs
+gulp.task('minify-css', () => {
+     // Folder with files to minify
+     return gulp.src('style.css')
+     //The method pipe() allow you to chain multiple tasks together 
+     //I execute the task to minify the files
+    .pipe(cleanCSS())
+    //I define the destination of the minified files with the method dest
+    .pipe(gulp.dest('dest'));
+});
+
+//We create a 'default' task that will run when we run `gulp` in the project
+gulp.task('default', function() {
+// We use `gulp.watch` for Gulp to expect changes in the files to run again
+  gulp.watch('style.min.css', function(evt) {
+  gulp.task('minify-css');
+  });
+});
