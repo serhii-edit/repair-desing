70779b055e8cdd0cd6854216cdb936437afe4f6a
Serhii Yakymenko <m.serhiiyakymenko@gmail.com>
2020-05-05 17:43:55 -0400

    include gulp & make gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+
+gulp.task('Hello', function(done) {
+  console.log('Hello, world!');
+  done();
+});
+
+// Static server
+gulp.task('browser-sync', function() {
+  browserSync.init({
+      server: {
+          baseDir: "./"
+      }
+  });
+  gulp.watch("./*.html").on('change', browserSync.reload);
+});
