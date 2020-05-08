8ebe12ecb7b5b9a3aa5e761fd50d16a9a58d7810
Serhii Yakymenko <m.serhiiyakymenko@gmail.com>
2020-05-08 14:34:49 -0400

    feat: added gulp-sass in gulpfile.js (but that did not work)

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,45 @@
-// const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
-
-
-let gulp = require('gulp');
-let cleanCSS = require('gulp-clean-css');
-
-gulp.task('minify-css', () => {
-  return gulp.src('css/*.css')
-    .pipe(cleanCSS({debug: true}, (details) => {
-      console.log(`${details.name}: ${details.stats.originalSize}`);
-      console.log(`${details.name}: ${details.stats.minifiedSize}`);
-    }))
-  .pipe(gulp.dest('css/minify'));
-});
+const gulp = require('src, dest, watch');
+const sass = require('gulp-sass');
 
 
 // Static server
-gulp.task('browser-sync', function() {
+function bs() {
+  serverSass();
   browserSync.init({
       server: {
           baseDir: "./"
       }
   });
-  gulp.watch("./*.html").on('change', browserSync.reload);
-});
+  watch("./*.html").on('change', browserSync.reload);
+  watch("./sass/**/*.sass", serveSass);
+  watch("./js/*.js").on('change', browserSync.reload);
+};
+// Browser Static server (end)
+
+
+// Compile sass into CSS & auto-inject into browsers
+function serveSass() {
+  return src("./sass/*.sass")
+      .pipe(sass())
+      .pipe(dest("./css"))
+      .pipe(browserSync.stream());
+};
+// Compile sass into css (end)
+
+exports.serve = bs;
+
+
+// Minify css file into minimize(save merrory)
+// let gulp = require('gulp');
+// let cleanCSS = require('gulp-clean-css');
+// gulp.task('minify-css', () => {
+//   return gulp.src('css/*.css')
+//     .pipe(cleanCSS({debug: true}, (details) => {
+//       console.log(`${details.name}: ${details.stats.originalSize}`);
+//       console.log(`${details.name}: ${details.stats.minifiedSize}`);
+//     }))
+//   .pipe(gulp.dest('css/minify'));
+// });
+// Minfify (end)
 
