d6505af6159241f2314cecc1cd76c4ed3f59d1f8
Serhii Yakymenko <m.serhiiyakymenko@gmail.com>
2020-05-18 18:32:28 -0400

    Feat: Added (js/main.js) and fucntion to modal(window)

---
+++ js/main.js
@@ -0,0 +1,19 @@
+const modal = document.querySelector('.modal');
+const modalBtn = document.querySelectorAll("[data-toggle=modal]")
+const CloseBtn = document.querySelector(".modal__close")
+
+const switchModal = () => {
+  modal.classList.toggle("modal_visible")
+}
+
+modalBtn.forEach(element => {
+  element.addEventListener("click", switchModal);
+});
+
+CloseBtn.addEventListener("click", switchModal);
+
+
+
+// document.addEventListener("DOMContentLoaded", fucntion(event){
+  
+// });
