ef88c80f0967a934f575f4c9a67015e6209f1d73
Serhii Yakymenko <m.serhiiyakymenko@gmail.com>
2020-05-19 17:18:03 -0400

    Fix: Reload all website, because fz problem

--- js/main.js
+++ js/main.js
@@ -1,15 +1,16 @@
 const modal = document.querySelector('.modal');
-const modalBtn = document.querySelectorAll("[data-toggle=modal]")
-const CloseBtn = document.querySelector(".modal__close")
+const modalBtn = document.querySelectorAll("[data-toggle=modal]");
+const CloseBtn = document.querySelector(".modal__close");
 
 const switchModal = () => {
-  modal.classList.toggle("modal_visible")
+  modal.classList.toggle("modal_visible");
 }
 
 modalBtn.forEach(element => {
   element.addEventListener("click", switchModal);
   CloseBtn.addEventListener("click", switchModal);
 });
+
 
 
 
