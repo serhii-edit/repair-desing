b7ea0737de7f7d19dae0c305816e1edee3243988
Serhii Yakymenko <m.serhiiyakymenko@gmail.com>
2020-05-20 14:04:57 -0400

    Fix: fixed the adaptive-css all document

--- js/main.js
+++ js/main.js
@@ -6,15 +6,23 @@ const switchModal = () => {
   modal.classList.toggle("modal_visible");
 }
 
+const outside = () => {
+  modal.classList.toggle("modal_outside");
+}
+
 modalBtn.forEach(element => {
   element.addEventListener("click", switchModal);
   CloseBtn.addEventListener("click", switchModal);
+  // window.addEventListener("click", outside);
 });
 
 
 
 
+// window.addEventListener("click", clickOutside);
 
-// document.addEventListener("DOMContentLoaded", fucntion(event){
-  
-// });
+// function clickOutside (event) {
+//   if (event.target == modal) {
+//     modal.classList.toggle(".modal-outside");
+//   }
+// }
